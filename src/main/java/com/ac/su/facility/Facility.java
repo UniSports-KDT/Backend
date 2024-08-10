a1c603a3701442d40c55a2c62f90eb098ef909b2
이정훈 <niko36@naver.com>
2024-08-10 12:34:52 +0900

    24.08.10 Facility.java 파일에 주석 추가

--- src/main/java/com/ac/su/facility/Facility.java
+++ src/main/java/com/ac/su/facility/Facility.java
@@ -13,10 +13,10 @@ public class Facility {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
-    private String description;
-    private String location;
-    private String operatingHours; //문자로 자유롭게 받음
+    private String name; //시설 이름
+    private String description; //시설 설명
+    private String location; //시설 위치
+    private String operatingHours; // 운영시간 예: "06:00-22:00"
     private Double fee;
     private LocalDateTime createdAt; // 생성 날짜
     @UpdateTimestamp
