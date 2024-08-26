7ca8844722d9a22cd834ba532c4d3b449a88fc09
이정훈 <niko36@naver.com>
2024-08-26 13:27:05 +0900

    24.08.26 S3 pADD resigned-urãl function

---
+++ src/main/java/com/ac/su/presignedurl/presignedUrlController.java
@@ -0,0 +1,20 @@
+package com.ac.su.presignedurl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class presignedUrlController {
+
+    private final S3Service s3Service;
+    @GetMapping("/presigned-url")
+    @ResponseBody
+    String getURL(@RequestParam String filename){
+        var result = s3Service.createPresignedUrl("test/" + filename);
+        return result;
+    }
+}
